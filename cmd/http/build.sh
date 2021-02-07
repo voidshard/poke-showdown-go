4011fe25fb99ac5f81ecdd5368fbbf5cbba1abc7
augustus <augustus@localhost.localdomain>
2021-02-07 14:08:27 +0000

    add comments to build script

--- cmd/http/build.sh
+++ cmd/http/build.sh
@@ -1,9 +1,25 @@
 #! /bin/sh
 
+#
+# Script builds docker image for a simple stateless HTTP server that wraps our sim logic.
+#
+
+set -eu
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
 
+# compile the app
+echo "compiling .."
 go build -o app main.go 
+
+# build the image
+echo "building image .."
 docker build -t poke-showdown-go .
 
+# remove the app
+echo "cleaning up .."
+rm -v app
+
+echo "done"
 cd -
